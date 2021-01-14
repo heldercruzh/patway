8e883e8b9115509e9afe60b131b405002aad9e79
heldercruzh <heldercruz@gmail.com>
2021-01-14 15:41:36 -0300

    update para a versão 11do angular

--- petwayfrontend/src/app/admin/admin-pages/admin-cliente/admin-listcliente/admin-listcliente.component.spec.ts
+++ petwayfrontend/src/app/admin/admin-pages/admin-cliente/admin-listcliente/admin-listcliente.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AdminListclienteComponent } from './admin-listcliente.component';
 
 describe('AdminListclienteComponent', () => {
   let component: AdminListclienteComponent;
   let fixture: ComponentFixture<AdminListclienteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminListclienteComponent ]
     })
--- petwayfrontend/src/app/shared/confirm-modal/confirm-modal.component.spec.ts
+++ petwayfrontend/src/app/shared/confirm-modal/confirm-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ComfirmModalComponent } from './confirm-modal.component';
 
@@ -6,7 +6,7 @@ describe('ComfirmModalComponent', () => {
   let component: ComfirmModalComponent;
   let fixture: ComponentFixture<ComfirmModalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ComfirmModalComponent ]
     })
