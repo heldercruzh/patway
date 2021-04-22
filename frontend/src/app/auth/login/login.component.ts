a46c8844790888730c7d197d5b39c7c08d8e2448
heldercruzh <heldercruz@gmail.com>
2021-04-21 23:59:43 -0300

    first commit

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -5,8 +5,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertModalService } from '../../shared/alert-modal.service';
 import { SocialAuthService } from "angularx-social-login";
-import { FacebookLoginProvider, GoogleLoginProvider, VKLoginProvider } from "angularx-social-login";
-import { SocialUser } from "angularx-social-login";
 
 @Component({
   selector: 'app-login',
@@ -20,7 +18,6 @@ export class LoginComponent implements OnInit {
   isLoginFailed = false;
   roles: string[] = [];
 
-  socialUser?: SocialUser;
   loggedIn= false;
   
   constructor (
@@ -33,24 +30,7 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
-    this.socialAuthService.authState.subscribe((socialUser) => {
-      this.socialUser = socialUser;
-      this.loggedIn = (socialUser != null);
-      console.log("0) "+this.loggedIn+
-      " provider: "+socialUser.provider+
-      " id: "+ socialUser.id+
-      " email: "+ socialUser.email+
-      " name: "+ socialUser.name+
-      " photoUrl: "+ socialUser.photoUrl+
-      " firstName: "+ socialUser.firstName+
-      " lastName: "+ socialUser.lastName+
-      " authToken: "+ socialUser.authToken+
-      " idToken: "+ socialUser.idToken+
-      " authorizationCode: "+ socialUser.authorizationCode+
-      " response: "+ socialUser.response)
-    });
-
+   
     this.clearForm();
     
     if (this.tokenStorage.getToken()) {
@@ -112,34 +92,5 @@ export class LoginComponent implements OnInit {
   private reloadPage(): void {
     window.location.reload();
   }
-
-  public signInWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
-      (socialUser) => {
-        //this.authService.retrieveToken(socialUser)
-        
-        this.authService.signInSocial(socialUser).subscribe(
-          token => {
-            console.log(token);
-            //this.tokenStorage.saveToken(data.token || '');
-            //this.tokenStorage.saveUser(data);
-            //this.isLoginFailed = false;
-            //this.loggedIn = true; System.out.println("teste");
-            //this.roles = this.tokenStorage.getUser().roles;
-            //this.reloadPage();
-          },
-          err => {
-            this.isLoginFailed = true;
-            this.clearForm();
-            this.modal.showAlertDanger(err.message);
-            console.log(err.message);
-          }
-        );
-      }
-    );
-  }
-
-  public signInWithFB(): void {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  }
+ 
 }
